982eb3a5b5bb476f7575be4b5f5ef6886f68a9d1
Jelena Adamović <105116909+adamoviccj@users.noreply.github.com>
2024-04-10 01:29:26 +0200

    model for application

---
+++ MoviesApp/MoviesApp/src/main/java/CloudComputingExam2024/MoviesApp/model/Movie.java
@@ -0,0 +1,13 @@
+package CloudComputingExam2024.MoviesApp.model;
+
+import lombok.Data;
+
+@Data
+public class Movie {
+    private Long Id;
+    private String Title;
+    private String Genre;
+    private Long Year;
+
+
+}
