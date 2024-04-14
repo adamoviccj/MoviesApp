85e48eff36f1b4530ee874ed5f01dbb91ddd58dc
Jelena Adamović <105116909+adamoviccj@users.noreply.github.com>
2024-04-14 14:48:39 +0200

    final changes

--- MoviesApp/MoviesApp/src/main/java/CloudComputingExam2024/MoviesApp/model/Film.java
+++ MoviesApp/MoviesApp/src/main/java/CloudComputingExam2024/MoviesApp/model/Film.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "films")
+//@Table(name = "films")
 public class Film {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
